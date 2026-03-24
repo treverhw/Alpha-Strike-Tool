6c588fb261cf3bbddd515d09e3d58870d34c3fcf
treverhw <trevertrades@gmail.com>
2026-03-24 12:13:20 -0700

    search options

--- UnitSearch.gd
+++ UnitSearch.gd
@@ -4,6 +4,10 @@ class_name UnitSearch
 @onready var UnitList : ItemList = get_node("VBoxContainer/UnitList")
 @onready var start: String = "res://Units/"
 @onready var timer: Timer = get_node("Timer")
+@onready var typeHbox: HBoxContainer = get_node("VBoxContainer/optionBoxes")
+@onready var classHbox: HBoxContainer = get_node("VBoxContainer/mechOptions")
+
+var currentOptions: Array = []
 
 var taskID = -1
 
@@ -15,11 +19,20 @@ func _ready() -> void:
 func _on_line_edit_text_changed(new_text: String) -> void:
 	curr = new_text
 	timer.start()
+	currentOptions = []
+	for option : CheckBox in typeHbox.get_children():
+		if option.button_pressed:
+			currentOptions.append(option.name)
+	var counter = 4
+	for option : CheckBox in classHbox.get_children():
+		if option.button_pressed:
+			currentOptions.append(counter)
+		counter -= 1
 
 func _on_timer_timeout() -> void:
 	WorkerThreadPool.wait_for_task_completion(taskID)
 	UnitList.clear()
-	if curr != "" and len(curr) >= 2:
+	if curr != "":
 		taskID = WorkerThreadPool.add_task(threadedSearch.bind(start,curr))
 
 func threadedSearch(path: String, text: String) -> void:
@@ -45,12 +58,17 @@ func loadUnits(path: String, text: String, files: Array[String]) -> void:
 
 func populateList(files: Array[String]) -> void:
 	print("Populating")
+	print(currentOptions)
+
 	for path in files:
 		if ".tres" in path or ".tres.remap" in path:
+			
 			var clean_path = path.replace(".remap", "")
 			var unit: Resource = load(clean_path)
+			print(currentOptions)
 			if is_instance_valid(unit):
-				addUnitToList(unit)
+				if unit.type in currentOptions and ("BM" not in currentOptions or unit.sz in currentOptions):
+					addUnitToList(unit)
 			else:
 				print("ERROR: " + clean_path + " Contains no unit")
 
@@ -61,3 +79,43 @@ func addUnitToList(unit: Resource) -> void:
 func _on_unit_list_item_activated(index: int) -> void:
 	var res = UnitList.get_item_metadata(index)
 	Global.Main.generateUnitCard(res)
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	_on_line_edit_text_changed(new_text)
+
+##Annoying tie together
+
+func _on_bm_pressed() -> void:
+	var mechOptions = get_node("VBoxContainer/mechOptions")
+	mechOptions.visible = !mechOptions.visible
+	if mechOptions.visible == true:
+		for size in mechOptions.get_children():
+			size.set_pressed_no_signal(true)
+	else:
+		for size in mechOptions.get_children():
+			size.set_pressed_no_signal(false)
+	_on_line_edit_text_changed(curr)
+
+func _on_cv_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_ba_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_ci_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_af_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_assault_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_heavy_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_medium_pressed() -> void:
+	_on_line_edit_text_changed(curr)
+
+func _on_light_pressed() -> void:
+	_on_line_edit_text_changed(curr)
